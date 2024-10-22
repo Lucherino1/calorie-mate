2640c5d704d1ba8686c80f9c42c49a27cf544c08
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-22 09:44:11 +0300

    CM-3: created DefaultLayout (layout with sidebar)

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex w-full bg-gray-ultra-light">
+    <header class="min-h-screen flex min-w-[290px] bg-white shadow-md">
+      <Sidebar />
+    </header>
+    <div class="flex flex-col flex-grow px-5 py-10">
+      <main class="flex-grow ">
+        Main
+      </main>
+      <footer class="">
+        Footer
+      </footer>
+    </div>
+  </div>
+</template>
