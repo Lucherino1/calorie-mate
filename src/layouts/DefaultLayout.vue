9d64118da207ef836f02133f9ea976085317ed69
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-22 21:55:58 +0300

    CM-3: refactor DefaultLayout component with a new Sidebar

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="flex w-full bg-gray-ultra-light">
-    <header class="min-h-screen flex min-w-[290px] bg-white shadow-md">
-      <Sidebar />
-    </header>
-    <div class="flex flex-col flex-grow px-5 py-10">
-      <main class="flex-grow ">
-        Main
-      </main>
-      <footer class="">
-        Footer
-      </footer>
-    </div>
+  <div class="grid grid-cols-[290px_1fr] bg-gray-ultra-light">
+    <Sidebar :menu-list="menuList" />
+
+    <slot name="main" />
   </div>
 </template>
+
+<script lang="ts" setup>
+defineProps<{
+  menuList: TSidebarMenuList
+}>()
+</script>
