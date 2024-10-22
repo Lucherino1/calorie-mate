ce15b8db827a088f3087ee471059ebd9cb0f9c98
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-22 17:46:20 +0300

    CM-3: implement routes for BlankLayout component

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,7 @@
 import type { RouteRecordRaw } from 'vue-router'
 import { routeNames } from './route-names'
 import { uiKitRoutes } from '@/views/ui-kit/ui-kit.routes'
+import BlankLayout from '@/layouts/BlankLayout.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -9,8 +10,15 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     path: '/',
-    name: routeNames.home,
-    component: () => import('@/views/home/Home.vue')
+    name: 'default',
+    component: BlankLayout,
+    children: [
+      {
+        path: '',
+        name: routeNames.home,
+        component: () => import('@/views/home/Home.vue')
+      }
+    ]
   },
 
   ...uiKitRoutes
