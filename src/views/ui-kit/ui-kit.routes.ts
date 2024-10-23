c733fad570f42c16254141ecfd4451f4942722d0
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-23 18:16:47 +0300

    CM-4: UI-kit fix by adding blank layout

--- src/views/ui-kit/ui-kit.routes.ts
+++ src/views/ui-kit/ui-kit.routes.ts
@@ -1,3 +1,4 @@
+import { ELayouts } from '@/types/enums'
 import type { RouteRecordRaw } from 'vue-router'
 
 export const uiKitRouteNames = {
@@ -8,6 +9,9 @@ export const uiKitRoutes: RouteRecordRaw[] = [
   {
     path: '/ui-kit',
     name: uiKitRouteNames.uiKit,
-    component: () => import('./UIKit.vue')
+    component: () => import('./UIKit.vue'),
+    meta: {
+      layout: ELayouts.blank
+    }
   }
 ]
