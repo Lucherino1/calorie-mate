1756d924d87c85ededb7ea32a15c65249e0b9f87
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-23 16:42:02 +0300

    CM-4: updated UI-kit routes

--- src/views/ui-kit/ui-kit.routes.ts
+++ src/views/ui-kit/ui-kit.routes.ts
@@ -1,14 +1,13 @@
 import type { RouteRecordRaw } from 'vue-router'
-import UIKit from '@/views/ui-kit/UIKit.vue'
 
-export const uiKitRoutesNames = {
-  uiKit: 'ui-kit'
+export const uiKitRouteNames = {
+  uiKit: 'uiKit'
 }
 
 export const uiKitRoutes: RouteRecordRaw[] = [
   {
     path: '/ui-kit',
-    name: uiKitRoutesNames.uiKit,
-    component: UIKit
+    name: uiKitRouteNames.uiKit,
+    component: () => import('./UIKit.vue')
   }
 ]
