d6df062cbd409ee7d37f2b23738761d8c57eac31
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-23 18:01:44 +0300

    CM-4: fix(router): redirect to /dashboard for root URL and unknown paths

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,12 +6,18 @@ import { authRoutes } from '@/views/auth/auth.routes'
 import { updateMealRoutes } from '@/views/update-meal/update-meal'
 import { recepiesAndProductsRoutes } from '@/views/recepies-and-products/recepies-and-products.routes'
 import { profileSettignsRoutes } from '@/views/profile-settings/profile-setting.routes'
+import { dashboardRoutes } from '@/views/dashboard/dashboard.routes'
 
 const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/dashboard'
+  },
   {
     path: '/:pathMatch(.*)*',
     redirect: '/'
   },
+  ...dashboardRoutes,
   ...uiKitRoutes,
   ...aboutUsRoutes,
   ...calculatorsRoutes,
