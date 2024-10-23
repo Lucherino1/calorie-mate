f732fbfa9271af054dff777b343040a52ca2741d
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-23 18:03:43 +0300

    CM-4: created AppLayout component and added it to App.vue to handle dynamic layouts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <router-view />
+  <AppLayout :layout="$route.meta.layout">
+    <router-view />
+  </AppLayout>
 </template>
 
 <script lang="ts" setup>
+import AppLayout from './layouts/AppLayout.vue'
 </script>
---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="layouts[props.layout]">
+    <slot />
+  </component>
+</template>
+
+<script lang="ts" setup>
+import { ELayouts } from '@/types/enums'
+import BlankLayout from './BlankLayout.vue'
+import DefaultLayout from './DefaultLayout.vue'
+
+const props = withDefaults(defineProps<{
+  layout: ELayouts
+}>(), {
+  layout: ELayouts.default
+})
+
+const layouts = {
+  [ELayouts.blank]: BlankLayout,
+  [ELayouts.default]: DefaultLayout
+}
+</script>
