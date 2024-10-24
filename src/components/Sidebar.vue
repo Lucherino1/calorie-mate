edc85a531fdfd77b2be67d5dd2c0e11ddd37e9bc
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-24 15:30:05 +0300

    CM-4: implemented icons in Sedebar and did minor changes to template

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,72 +3,112 @@
     <div class="pt-14 pb-12 font-poppins text-[26px] leading-[26px] text-primary-dark text-center border-b">
       <h1><b>CALORIE</b>MATE</h1>
     </div>
-    <div class="pl-9 pt-10">
+    <div class="pl-5 pt-10">
       <ul class="flex flex-col gap-5">
         <li
           v-for="item in menuList"
           :key="item.routeName"
-          class="font-medium leading-[30px] block w-full
+          class="font-medium leading-[30px] w-full block
           text-gray-light hover:font-medium hover:text-primary-dark relative"
         >
           <router-link
-            class="w-full block"
+            class="w-full truncate flex items-center"
             :to="{ name:item.routeName }"
-            active-class="sidebar__link--active"
+            active-class="dsidebar__link--active"
           >
+            <component :is="item.icon" class="mr-2 menu-icon" />
             {{ item.label }}
           </router-link>
         </li>
       </ul>
+      <div class="pr-9 mt-20 flex flex-col w-full">
+        <el-button @click="logout">Logout</el-button>
+      </div>
     </div>
   </aside>
 </template>
 
 <script lang="ts" setup>
-import { routeNames } from '@/router/route-names'
+import IconHome from '~icons/icon/home'
+import IconProfile from '~icons/icon/profile'
+import IconSignIn from '~icons/icon/sign-in'
+import IconRecepiesAndProducts from '~icons/icon/recepies-and-products'
+import IconAdjustMeal from '~icons/icon/adjust-meal'
+import IconHealthAndNutrition from '~icons/icon/health-and-nutrition'
+import IconApprove from '~icons/icon/approve'
+import IconAboutUs from '~icons/icon/about-us'
+import IconSignUp from '~icons/icon/sign-up'
 
-type TRouteNames = keyof typeof routeNames
+import type { FunctionalComponent } from 'vue'
 
 interface ISidebarMenuList {
   label: string
+  icon?: FunctionalComponent
   routeName: TRouteNames
 }
 
 const menuList: ISidebarMenuList[] = [
   {
     label: 'Dashboard',
-    routeName: 'dashboard'
+    routeName: 'dashboard',
+    icon: IconHome
   },
   {
     label: 'Profile',
-    routeName: 'profileSettings'
+    routeName: 'profileSettings',
+    icon: IconProfile
+  },
+  {
+    label: 'Sign Up',
+    routeName: 'signup',
+    icon: IconSignUp
+  },
+  {
+    label: 'Sign In',
+    routeName: 'signin',
+    icon: IconSignIn
   },
   {
     label: 'Adjust Meal',
-    routeName: 'updateMeal'
+    routeName: 'updateMeal',
+    icon: IconAdjustMeal
   },
   {
     label: 'Recepies and products',
-    routeName: 'recepiesAndProducts'
+    routeName: 'recepiesAndProducts',
+    icon: IconRecepiesAndProducts
+  },
+  {
+    label: 'Approve products and recepies',
+    routeName: 'productRecipeApproval',
+    icon: IconApprove
   },
   {
     label: 'Health & Nutrition Calculators',
-    routeName: 'calculators'
+    routeName: 'calculators',
+    icon: IconHealthAndNutrition
   },
   {
     label: 'Get to Know Us',
-    routeName: 'aboutUs'
+    routeName: 'aboutUs',
+    icon: IconAboutUs
   }
 ]
+// will be done in further
+function logout () {}
 </script>
 
 <style lang="scss" scoped>
 .sidebar__link--active {
+
+.menu-icon {
+  @apply fill-primary
+}
   &::after {
     content: '';
     @apply block absolute right-0 top-1/2 transform -translate-y-1/2 w-1 h-9 bg-primary rounded-full;
   };
 
-  @apply font-bold text-primary-dark;
+  @apply font-bold text-primary-dark fill-primary;
 }
 </style>
