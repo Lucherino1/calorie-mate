f3c3d3875aadb1f1acd28b2145817a37c280678c
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-25 10:26:21 +0300

    CM-5: implemented inputs shared components

---
+++ src/components/ProfileInputs.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-form-item label="First name" prop="firstName">
+      <el-input v-model="localModel.firstName" placeholder="Anthony" type="text" />
+    </el-form-item>
+
+    <el-form-item label="Last name" prop="lastName">
+      <el-input v-model="localModel.lastName" placeholder="Soprano" type="text" />
+    </el-form-item>
+
+    <el-form-item label="Email" prop="email">
+      <el-input v-model="localModel.email" placeholder="email@example.com" type="email" />
+    </el-form-item>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  model: IProfileFields
+}>()
+
+const localModel = reactive({ ...props.model })
+</script>
