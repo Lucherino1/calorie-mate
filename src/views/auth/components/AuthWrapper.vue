2e8c81423070f1e3b34ca6869ac33fc994d8f3a2
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-25 20:10:13 +0300

    CM-5: created wrapper for Auth components

---
+++ src/views/auth/components/AuthWrapper.vue
@@ -0,0 +1,59 @@
+<template>
+  <main class="max-w-[1728px] mx-auto w-full" :loading="loadingStatus">
+    <div class="flex relative">
+      <div class="flex-1 flex justify-center items-center">
+        <div class="flex flex-col gap-2 w-full max-w-[410px] px-2">
+          <h2 class="text-4xl leading-[56px] font-bold text-primary-dark">{{ titleText }}</h2>
+          <slot />
+          <div class="flex flex-col gap-1 mt-1">
+            <div class="flex gap-1">
+              <p class="text-primary-dark">{{ label }}</p>
+              <el-button link :type="$elComponentType.primary" @click="toggleAuthPage">
+                {{ toggleAuthPageBtnText }}
+              </el-button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="flex-1 flex justify-center px-[5px] bg-[url('/public/auth-bg.jpg')]
+        bg-cover bg-center h-screen rounded-bl-[120px] font-poppins"
+      >
+        <div class="flex flex-col w-full max-w-[410px] justify-center items-center">
+          <div class="flex-shrink-0 h-[280px] w-[280px] mb-[40px]">
+            <img
+              class=" object-cover"
+              src="/public/logo.png"
+              alt="logo"
+            >
+          </div>
+          <div class="flex justify-center items-center text-center mb-[100px]">
+            <h1 class="text-5xl text-white"><b>CALORIE</b> MATE</h1>
+          </div>
+          <button
+            class="text-white text-lg leading-[18px] px-10 lg:px-[110px]
+             py-[20px] border-2 border-gray-light rounded-[28px] sm:px-1 md:px-5
+            hover:bg-white hover:text-primary transition-all duration-300"
+          >
+            <p class="text-nowrap leading-10"><b>Click here</b><br>to learn more about us</p>
+          </button>
+        </div>
+      </div>
+      <div class="absolute bottom-10 left-[230px]">
+        <p class="font-medium text-sm leading-6 text-gray-light text-nowrap">
+          © 2024 Calories Mate. All Rights Reserved. Made with love by <b>Good People</b>.
+        </p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  loadingStatus: boolean
+  titleText: string
+  label: string
+  toggleAuthPageBtnText: string
+  toggleAuthPage: () => void
+}>()
+</script>
