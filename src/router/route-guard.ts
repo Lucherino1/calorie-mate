f3f7c545c3da6d19c7a6798f195948fc05b2adff
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-27 01:21:33 +0300

    CM-5: implemented route guard rules

--- src/router/route-guard.ts
+++ src/router/route-guard.ts
@@ -11,5 +11,46 @@ export const routeGuard = async (
   if (to.meta.requireAuth && !store.user) {
     return next({ name: routeNames.signin })
   }
+
+  if (to.meta.roles) {
+    const userRole = store.user?.role
+    const allowedRoles = to.meta.roles
+    if (!userRole || !allowedRoles.includes(userRole)) {
+      return next({ name: routeNames.dashboard })
+    }
+  }
+
   next()
+}
+
+export const renewSession = async (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  const store = useAuthStore()
+  const { user } = storeToRefs(store)
+  const { getUserData } = store
+
+  if (!store.user) {
+    const userData = await getUserData()
+    if (userData) {
+      user.value = userData
+    }
+  }
+  next()
+}
+
+export const sessionGuard = async (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  const store = useAuthStore()
+
+  if (store.user && to.meta.isProtectedDuringSession) {
+    next({ name: routeNames.dashboard })
+  } else {
+    next()
+  }
 }
