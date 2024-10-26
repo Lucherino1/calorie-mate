5a05e3fa1e21cb4ce44b6c911e79d8935b2334b9
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-27 01:18:20 +0300

    CM-5: typed supabase client

--- src/composables/supabase/index.ts
+++ src/composables/supabase/index.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
+const supabase = createClient<IDatabase<IBodyDetails, INutritionDetails, ITargetNutritionDetailsByMeal>['public']['Tables']['profiles']['Row']>(
   import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY
 )
 
