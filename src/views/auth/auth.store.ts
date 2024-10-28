22ba91240d3bea97041308f76a33caaee9e8c88a
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-28 19:19:09 +0200

    CM-5: implemented getting user data method via getting by id or without it

--- src/views/auth/auth.store.ts
+++ src/views/auth/auth.store.ts
@@ -4,14 +4,14 @@ import { routeNames } from '@/router/route-names'
 import { showWarningNotification } from '@/helpers'
 
 export const useAuthStore = defineStore('authStore', () => {
-  const user = ref<TNullable<TUser>>(null)
+  const user = ref<TNullable<IUser>>(null)
 
   const signup = async (payload: ISignUpPayload) => {
     try {
       const { data, error } = await authService.signup(payload)
 
       if (error || !data.user) {
-        throw new Error(error?.message)
+        throw new Error(error?.message || 'Signup failed')
       }
 
       router.push({ name: routeNames.signin })
@@ -28,8 +28,7 @@ export const useAuthStore = defineStore('authStore', () => {
         throw new Error(error?.message || 'Sign In failed')
       }
 
-      const userData = await getUserDataById(data.user.id)
-
+      const userData = await getUserProfile(data.user.id)
       user.value = userData
 
       router.push({ name: routeNames.dashboard })
@@ -42,39 +41,25 @@ export const useAuthStore = defineStore('authStore', () => {
     try {
       const { error } = await authService.signout()
       if (error) {
-        showWarningNotification(error.message, 'Signout Error')
+        throw new Error(error.message || 'Signout failed')
       }
 
       user.value = null
     } catch (error) {
-      console.error(error)
+      showWarningNotification((error as Error).message, 'Signout Error')
     }
 
     window.location.href = router.resolve({ name: routeNames.signin }).href
   }
 
-  const getUserDataById = async (userId: string): Promise<TUser | null> => {
-    try {
-      const { data, error } = await authService.getUserById(userId)
-      if (error) throw new Error(error.message)
+  const getUserProfile = async (userId?: string): Promise<IUser | null> => {
+    const id = userId || (await authService.getUser())?.id
+    if (!id) return null
 
-      return data
-    } catch (error) {
-      showWarningNotification((error as Error).message, 'User Data Retrieval Error')
-      return null
-    }
-  }
-
-  const getUserData = async (): Promise<TUser | null> => {
-    const userId = (await authService.getUser())?.id
-    if (!userId) return null
-
-    const { data, error } = await authService.getUserById(userId)
-
+    const { data, error } = await authService.getUserById(id)
     if (error) {
-      await signout()
-      showWarningNotification(error.message, 'Session Error')
-      return null
+      if (!userId) await signout()
+      throw new Error(error.message || 'User Data Retrieval failed')
     }
 
     return data
@@ -85,7 +70,6 @@ export const useAuthStore = defineStore('authStore', () => {
     signup,
     signin,
     signout,
-    getUserDataById,
-    getUserData
+    getUserProfile
   }
 })
