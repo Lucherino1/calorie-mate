aa533b12aff1d983eda25c60347f7bfb89cc785d
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-31 00:56:23 +0200

    CM-7: implemented method to redirect User to updateMeal page with mealType in route params

--- src/views/dashboard/components/MealCard.vue
+++ src/views/dashboard/components/MealCard.vue
@@ -1,27 +1,54 @@
 <template>
   <el-card class="flex flex-col items-center text-center">
-    <p class="font-bold text-lg">Breakfast</p>
-    <el-progress type="circle" :percentage="75">
+    <p class="font-bold text-lg">{{ label }}</p>
+    <el-progress class="py-[10px]" type="circle" :percentage="percentage">
       <template #default>
-        <div class="w-[55px] h-[55px] mx-auto">
+        <div class="w-[55px] h-[55px] mx-auto relative">
           <img
-            class="bg-gray w-full h-auto object-cover"
-            :src="imageSrc"
-            srcset="/src/assets/images/meals/breakfast.png 1x, /src/assets/images/meals/breakfast@2.png 2x"
-            alt="Meal"
+            :src="image"
+            class="bg-gray overflow-hidden w-full h-auto object-cover"
+            :alt="label"
             loading="lazy"
           >
+          <div class="w-[30px] h-[30px] absolute left-12">
+            <el-button circle type="info" @click="redirectToUpdateMeal">
+              <IconPlus />
+            </el-button>
+          </div>
         </div>
       </template>
     </el-progress>
-    <p class="font-semibold text-lg">568 cal</p>
-    <p class="text-sm text-gray-500">6 items</p>
+    <p class="font-semibold text-lg">
+      {{ caloriesConsumed }}
+      <span class="font-thin"> cal</span>
+    </p>
+    <p class="text-sm text-gray-500">
+      {{ itemsCount }}
+      <span>{{ countedItemsText }}</span>
+    </p>
   </el-card>
 </template>
 
 <script lang="ts" setup>
+import { routeNames } from '@/router/route-names'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
 const props = defineProps<{
-  imageSrc: string
+  label: string
   percentage: number
+  caloriesConsumed: number
+  itemsCount: number
+  image: string
+  mealType: string
 }>()
+
+const countedItemsText = computed(() => {
+  return props.itemsCount === 1 ? 'item' : 'items'
+})
+
+const redirectToUpdateMeal = () => {
+  router.push({ name: routeNames.updateMeal, params: { mealType: props.mealType } })
+}
 </script>
