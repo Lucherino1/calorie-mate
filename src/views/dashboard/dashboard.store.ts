ebaf2f8f04f6f4d31467dedc452571b989432755
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-31 00:54:37 +0200

    CM-7: added dashboard store and implemented methods for getting and updating user dashboard data

---
+++ src/views/dashboard/dashboard.store.ts
@@ -0,0 +1,46 @@
+import { defineStore } from 'pinia'
+
+export const useDashboardStore = defineStore('dashboardStore', () => {
+  const dashboardPageLoading = ref(false)
+  const authStore = useAuthStore()
+  const userId = computed(() => authStore.user?.id)
+
+  const date = ref(new Date().toISOString().split('T')[0])
+  const userDashboard = ref<TNullable<IDashboard>>(null)
+
+  const calsEaten = computed(() => nutritionService.calcTotalNutritious(userDashboard.value))
+  const calsAndItemsEatenByMeal = computed(() => nutritionService.calcNutritiousByMeals(userDashboard.value))
+
+  const getUserDashboard = async (selectedDate: string) => {
+    dashboardPageLoading.value = true
+    if (!userId.value) return
+
+    const { data, error } = await dashboardService.getUserDashboard(selectedDate)
+    if (error) {
+      console.error('Error fetching user dashboard:', error.message)
+      dashboardPageLoading.value = false
+      return
+    }
+
+    if (data && data.length > 0) {
+      userDashboard.value = data[0]
+      dashboardPageLoading.value = false
+    } else {
+      try {
+        const newDashboard = await dashboardService.createEmptyDashboard(selectedDate, userId.value)
+        userDashboard.value = newDashboard
+      } catch (error) {
+        console.error('Error creating a new dashboard:', error)
+      }
+    }
+    dashboardPageLoading.value = false
+  }
+  return {
+    date,
+    userDashboard,
+    getUserDashboard,
+    dashboardPageLoading,
+    calsEaten,
+    calsAndItemsEatenByMeal
+  }
+})
