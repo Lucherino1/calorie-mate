411ee41678619fdb662c241d248eea19a3624253
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-31 00:59:23 +0200

    CM-7: added page loading logic, and implemented methods for getting user's Dashboard, updating user's current weight and display Dashboard data on page

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,343 +1,276 @@
 <template>
-  <div class="dashboard-container">
-    <div class="flex justify-between">
-      <div class="flex flex-col justify-between">
-        <p class="font-bold text-gray-light text-[34px] leading-10">
-          Hello, <span class="text-primary-dark">{{ userName }}!</span>
-        </p>
-        <div class="date-picker-wrapper w-full flex justify-between">
-          <el-date-picker
-            v-model="date"
-            type="date"
-            :clearable="false"
-            start-placeholder="true"
-            :size="$elComponentSize.large"
-          />
-        </div>
-      </div>
-      <el-card class="flex justify-center items-center max-w-[300px] h-[150px] weight-form-wrapper">
-        <el-form
-          ref="formRef"
-          :model="currentWeightFormModel"
+  <div v-loading.fullscreen="dashboardPageLoading" class="dashboard-container">
+    <div class="flex flex-col gap-5">
+      <p class="font-bold text-gray-light text-[34px] leading-10">
+        Hello, <span class="text-primary-dark">{{ user?.firstName }}!</span>
+      </p>
+      <div class="date-picker-wrapper w-full flex justify-between">
+        <el-date-picker
+          v-model="date"
+          type="date"
+          :clearable="false"
+          start-placeholder="true"
           :size="$elComponentSize.large"
-          label-position="top"
-        >
-          <el-form-item label="Your weight is:" prop="currentWeight">
-            <el-input-number
-              v-model="currentWeightFormModel.currentWeight"
-              :precision="1"
-              :step="0.1"
-            />
-          </el-form-item>
-          <div v-if="isWeightEditMode" class="flex items-center justify-end">
-            <el-form-item class="flex items-center justify-end">
-              <el-button :type="$elComponentType.danger" link>
-                Cancel
-              </el-button>
-              <el-button native-type="submit" :type="$elComponentType.info" link>
-                Save
-              </el-button>
-            </el-form-item>
-          </div>
-        </el-form>
-      </el-card>
+          @change="handleDateChange"
+        />
+      </div>
     </div>
-    <div class="flex gap-5 mt-10">
-      <el-card class="w-full h-auto flex-1">
+    <div class="flex gap-5 mt-10 flex-wrap">
+      <el-card class="w-full h-auto flex-1 min-w-[310px]">
         <p class="card-header">Total Calories:</p>
         <div class="flex justify-center items-center">
           <el-progress
             type="dashboard"
             class="py-8"
-            :percentage="caloriesProgressPercentage"
+            :percentage="nutrientPersentage.calories"
             :stroke-width="12"
-            :width="200"
-            :color="colors"
+            :width="220"
           >
             <template #default>
-              LEFT
+              <p class="text-gray-light leading-7">
+                <b class="text-primary-dark">{{ calculatedCalsRemaining }}</b>
+                <br> cal left
+              </p>
             </template>
           </el-progress>
         </div>
       </el-card>
       <div class="flex flex-1 flex-col w-full justify-between">
-        <el-card class="text-sm text-dark">
-          <p class="text-primary-dark font-semibold">Carbs:</p>
-          <el-progress class="py-[2px]" text-inside :stroke-width="15" :percentage="70" status="success" />
-          <p class="text-sm text-gray-light">0/{{ userTargetNutrition.carbs }}g</p>
+        <NutrientCard
+          v-for="nutrient in nutrientData"
+          :key="nutrient.label"
+          :label="nutrient.label"
+          :percentage="nutrient.percentage"
+          :consumedAmount="nutrient.consumedAmount"
+          :targetAmount="nutrient.targetAmount"
+          :progress-color-type="nutrient.progressColorType"
+        />
+      </div>
+      <div class="flex-1 gap-5 flex flex-col">
+        <el-card class="flex justify-center text-center items-center min-h-[155px] weight-form-wrapper">
+          <el-form
+            ref="formRef"
+            class=""
+            :model="bodyDetailsFormModel"
+            :rules="formRules"
+            :size="$elComponentSize.large"
+            label-position="top"
+            hide-required-asterisk
+            @submit.prevent="submit"
+          >
+            <el-form-item label="Your weight is:" prop="currentWeight">
+              <el-input-number
+                v-model="bodyDetailsFormModel.currentWeight"
+                :precision="1"
+                :step="0.1"
+                @input="isEditWeightMode = true"
+              >
+                <template #suffix>kg</template>
+              </el-input-number>
+            </el-form-item>
+            <transition name="fade">
+              <div v-show="isEditWeightMode" class="flex items-center justify-center">
+                <el-form-item class="flex items-center justify-end">
+                  <el-button :type="$elComponentType.danger" link @click="isEditWeightMode = false">
+                    Cancel
+                  </el-button>
+                  <el-button native-type="submit" :type="$elComponentType.info" link>
+                    Save
+                  </el-button>
+                </el-form-item>
+              </div>
+            </transition>
+          </el-form>
         </el-card>
-        <el-card class="text-sm text-dark">
-          <p class="text-primary-dark font-semibold">Proteins:</p>
-          <el-progress class="py-[2px]" text-inside :stroke-width="15" :percentage="78" status="warning" />
-          <p class="text-sm text-gray-light">0/{{ userTargetNutrition.proteins }}g</p>
-        </el-card>
-        <el-card class="text-sm text-dark">
-          <p class="text-primary-dark font-semibold">Fats:</p>
-          <el-progress class="py-[2px]" text-inside :stroke-width="15" :percentage="20" status="exception" />
-          <p class="text-sm text-gray-light">0/{{ userTargetNutrition.fats }}g</p>
+        <el-card
+          class="w-full h-auto flex-1"
+        >
+          <div class="text-[34px] leading-10">
+            <p class="card-header">Hydration:</p>
+          </div>
         </el-card>
       </div>
-      <el-card
-        class="w-full h-auto flex-1"
-      >
-        <div class="text-[34px] leading-10">
-          <p class="card-header">Hydration:</p>
-        </div>
-      </el-card>
     </div>
     <div class="grid grid-cols-4 gap-5 mt-10">
-      <el-card class="flex flex-col items-center text-center">
-        <p class="font-bold text-lg">Breakfast</p>
-        <el-progress type="circle" :percentage="75">
-          <template #default>
-            <div class="w-[55px] h-[55px] mx-auto">
-              <img
-                class="bg-gray overflow-hidden w-full h-auto object-cover"
-                src="/src/assets/images/meals/breakfast.png"
-                srcset="/src/assets/images/meals/breakfast.png 1x, /src/assets/images/meals/breakfast@2.png 2x"
-                alt="Meal"
-                loading="lazy"
-              >
-            </div>
-          </template>
-        </el-progress>
-        <p class="font-semibold text-lg">568 cal</p>
-        <p class="text-sm text-gray-500">6 items</p>
-      </el-card>
-      <el-card class="flex flex-col items-center text-center">
-        <p class="font-bold text-lg">Breakfast</p>
-        <el-progress type="circle" :percentage="75">
-          <template #default>
-            <div class="w-[55px] h-[55px] mx-auto">
-              <img
-                class="bg-gray overflow-hidden w-full h-auto object-cover"
-                src="/src/assets/images/meals/breakfast.png"
-                srcset="/src/assets/images/meals/breakfast.png 1x, /src/assets/images/meals/breakfast@2.png 2x"
-                alt="Meal"
-                loading="lazy"
-              >
-            </div>
-          </template>
-        </el-progress>
-        <p class="font-semibold text-lg">568 cal</p>
-        <p class="text-sm text-gray-500">6 items</p>
-      </el-card>
-      <el-card class="flex flex-col items-center text-center">
-        <p class="font-bold text-lg">Breakfast</p>
-        <el-progress type="circle" :percentage="75">
-          <template #default>
-            <div class="w-[55px] h-[55px] mx-auto">
-              <img
-                class="bg-gray overflow-hidden w-full h-auto object-cover"
-                src="/src/assets/images/meals/breakfast.png"
-                srcset="/src/assets/images/meals/breakfast.png 1x, /src/assets/images/meals/breakfast@2.png 2x"
-                alt="Meal"
-                loading="lazy"
-              >
-            </div>
-          </template>
-        </el-progress>
-        <p class="font-semibold text-lg">568 cal</p>
-        <p class="text-sm text-gray-500">6 items</p>
-      </el-card>
-      <el-card class="flex flex-col items-center text-center">
-        <p class="font-bold text-lg">Breakfast</p>
-        <el-progress type="circle" :percentage="75">
-          <template #default>
-            <div class="w-[55px] h-[55px] mx-auto">
-              <img
-                class="bg-gray w-full h-auto object-cover"
-                src="/src/assets/images/meals/breakfast.png"
-                srcset="/src/assets/images/meals/breakfast.png 1x, /src/assets/images/meals/breakfast@2.png 2x"
-                alt="Meal"
-                loading="lazy"
-              >
-            </div>
-          </template>
-        </el-progress>
-        <p class="font-semibold text-lg">568 cal</p>
-        <p class="text-sm text-gray-500">6 items</p>
-      </el-card>
+      <MealCard
+        v-for="meal in mealData"
+        :key="meal.label"
+        :label="meal.label"
+        :percentage="meal.percentage || 0"
+        :caloriesConsumed="meal.caloriesConsumed"
+        :itemsCount="meal.countedItems"
+        :image="meal.image"
+        :meal-type="meal.mealType"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { EProgressColorStatus } from '@/views/dashboard/dashboard.enums'
+
+const dashboardStore = useDashboardStore()
+const { getUserDashboard } = dashboardStore
+const { userDashboard, date, dashboardPageLoading, calsEaten, calsAndItemsEatenByMeal } = storeToRefs(dashboardStore)
+
 const authStore = useAuthStore()
 const { user } = storeToRefs(authStore)
 
-const userName = computed(() => user.value?.firstName)
+const handleDateChange = (newDate: string) => {
+  const selectedDate = new Date(newDate)
 
-const userTargetNutrition = computed(() => {
-  return { ...user.value?.targetNutritionDetails }
-})
+  const year = selectedDate.getFullYear()
+  const month = String(selectedDate.getMonth() + 1).padStart(2, '0')
+  const day = String(selectedDate.getDate()).padStart(2, '0')
 
-const userTargetNutritionByMeal = computed(() => {
-  return { ...user.value?.targetNutritionDetailsByMeal }
-})
-
-const date = ref(new Date().toISOString().split('T')[0])
-
-const isWeightEditMode = computed(() => {
-  if (currentWeightFormModel.currentWeight !== user.value?.bodyDetails.currentWeight) {
-    return true
-  }
-  return false
-})
+  date.value = `${year}-${month}-${day}`
+  getUserDashboard(date.value)
+}
 
 const formRef = useTemplateRef<TElementPlus['FormInstance']>('formRef')
 
-const currentWeightFormModel = reactive<ICurrentWeight>({
-  currentWeight: user.value?.bodyDetails.currentWeight
+const formRules = reactive({
+  age: [useRequiredRule(), useMinAgeRule()],
+  sex: useRequiredRule(),
+  activityLevel: useRequiredRule(),
+  height: [useRequiredRule(), useHeightRangeRule()],
+  currentWeight: [useRequiredRule(), useCurrentWeightRule()],
+  goalWeight: [
+    useRequiredRule(),
+    useGoalWeightRule()
+  ]
 })
 
-// Calories
-const colors = [
-  { color: '#f56c6c', percentage: 20 },
-  { color: '#e6a23c', percentage: 40 },
-  { color: '#5cb87a', percentage: 60 },
-  { color: '#1989fa', percentage: 80 },
-  { color: '#4318ff', percentage: 100 }
-]
+const bodyDetailsFormModel = reactive<IBodyDetails>({
+  age: user.value?.bodyDetails.age ?? 0,
+  sex: user.value?.bodyDetails.sex ?? 'male',
+  activityLevel: user.value?.bodyDetails.activityLevel ?? 1.2,
+  height: user.value?.bodyDetails.height ?? 0,
+  currentWeight: user.value?.bodyDetails.currentWeight ?? 0,
+  goalWeight: user.value?.bodyDetails.goalWeight ?? 0
+})
 
-// Test for calories progress
-const caloriesProgressPercentage = ref(90)
+const isEditWeightMode = ref(false)
 
-// MOCK!!!!!
-const mockDashboards: IDashboard = {
-  id: '1',
-  date: '2024-10-01',
-  userId: 'user_123',
-  waterAmount: 2000,
-  meals: [
-    {
-      products: [
-        {
-          id: 'product_1',
-          name: 'Apple',
-          nutritionDetails: {
-            calories: 52,
-            fats: 0.2,
-            carbs: 14,
-            proteins: 0.3
-          },
-          type: 'fruit',
-          isVegan: true,
-          userId: '',
-          isUnderReview: false
-        },
-        {
-          id: 'product_2',
-          name: 'Peanut Butter',
-          nutritionDetails: {
-            calories: 94,
-            fats: 8.2,
-            carbs: 3,
-            proteins: 4
-          },
-          type: 'nut',
-          isVegan: true,
-          userId: '',
-          isUnderReview: false
+function submit () {
+  isEditWeightMode.value = false
+  formRef.value?.validate(async (isValid: boolean) => {
+    if (!bodyDetailsFormModel) return
+    if (isValid) {
+      try {
+        if (!user.value?.id) {
+          return
         }
-      ],
-      recipes: [
-        {
-          id: 'recipe_1',
-          name: 'Vegan Salad',
-          userId: 'user_123',
-          description: 'A refreshing vegan salad with mixed greens and nuts.',
-          image: 'salad.jpg',
-          portionWeight: 150,
-          type: 'salad',
-          isVegan: true,
-          ingredients: [
-            {
-              id: 'product_3',
-              name: 'Lettuce',
-              nutritionDetails: {
-                calories: 15,
-                fats: 0.1,
-                carbs: 3,
-                proteins: 1
-              },
-              type: 'vegetable',
-              isVegan: true,
-              userId: '',
-              isUnderReview: false
-            }
-          ],
-          isUnderReview: false
-        }
-      ],
-      type: 'breakfast'
-    },
-    {
-      products: [
-        {
-          id: 'product_1',
-          name: 'Apple',
-          nutritionDetails: {
-            calories: 52,
-            fats: 0.2,
-            carbs: 14,
-            proteins: 0.3
-          },
-          type: 'fruit',
-          isVegan: true,
-          userId: '',
-          isUnderReview: false
-        },
-        {
-          id: 'product_2',
-          name: 'Peanut Butter',
-          nutritionDetails: {
-            calories: 94,
-            fats: 8.2,
-            carbs: 3,
-            proteins: 4
-          },
-          type: 'nut',
-          isVegan: true,
-          userId: '',
-          isUnderReview: false
-        }
-      ],
-      recipes: [
-        {
-          id: 'recipe_1',
-          name: 'Vegan Salad',
-          userId: 'user_123',
-          description: 'A refreshing vegan salad with mixed greens and nuts.',
-          image: 'salad.jpg',
-          portionWeight: 150,
-          type: 'salad',
-          isVegan: true,
-          ingredients: [
-            {
-              id: 'product_3',
-              name: 'Lettuce',
-              nutritionDetails: {
-                calories: 15,
-                fats: 0.1,
-                carbs: 3,
-                proteins: 1
-              },
-              type: 'vegetable',
-              isVegan: true,
-              userId: '',
-              isUnderReview: false
-            }
-          ],
-          isUnderReview: false
-        }
-      ],
-      type: 'snacks'
+        profileService.recalculateTargetNutrition(user.value)
+
+        await profileService.updateUserBodyDetails(user.value?.id, { ...bodyDetailsFormModel })
+
+        user.value.bodyDetails = bodyDetailsFormModel
+      } catch (error) {
+        console.error(error)
+      }
     }
-  ]
+  })
 }
 
-console.log(nutritionService.calculateMealCaloriesPercentage(mockDashboards, userTargetNutritionByMeal.value))
+const userTargetNutrition = computed(() => {
+  if (user.value?.targetNutritionDetails !== undefined) {
+    return { ...user.value?.targetNutritionDetails }
+  }
+  return {
+    calories: 0,
+    carbs: 0,
+    proteins: 0,
+    fats: 0
+  }
+})
+
+const userTargetNutritionByMeal = computed(() => {
+  if (user.value === undefined) return {}
+  return { ...user.value?.targetNutritionDetailsByMeal }
+})
+
+const calculatedCalsRemaining = computed(() => {
+  return nutritionService.calcRemainingCalories(userDashboard.value, userTargetNutrition.value.calories)
+})
+
+const nutrientPersentage = computed(() => {
+  return nutritionService.calcNutrientPercentage(userDashboard.value, userTargetNutrition.value)
+})
+
+const nutrientData = computed(() => {
+  return [
+    {
+      label: 'Carbs',
+      percentage: nutrientPersentage.value.carbs,
+      targetAmount: userTargetNutrition.value.carbs,
+      consumedAmount: calsEaten.value.carbs,
+      image: '/src/assets/images/nutrients/carbs.png',
+      progressColorType: EProgressColorStatus.success
+    },
+    {
+      label: 'Proteins',
+      percentage: nutrientPersentage.value.proteins,
+      targetAmount: userTargetNutrition.value.proteins,
+      consumedAmount: calsEaten.value.proteins,
+      image: '/src/assets/images/nutrients/proteins.png',
+      progressColorType: EProgressColorStatus.warning
+    },
+    {
+      label: 'Fats',
+      percentage: nutrientPersentage.value.fats,
+      targetAmount: userTargetNutrition.value.fats,
+      consumedAmount: calsEaten.value.fats,
+      image: '/src/assets/images/nutrients/fats.png',
+      progressColorType: EProgressColorStatus.exception
+    }
+  ]
+})
+
+const mealData = computed(() => {
+  const percentage = nutritionService.calcMealCaloriesPercentage(userDashboard.value, userTargetNutritionByMeal.value)
+
+  return [
+    {
+      label: 'Breakfast',
+      mealType: 'breakfast',
+      percentage: percentage.breakfast,
+      caloriesConsumed: calsAndItemsEatenByMeal.value.breakfast.calories,
+      image: '/src/assets/images/meals/breakfast.png',
+      countedItems: calsAndItemsEatenByMeal.value.breakfast.itemsCount
+    },
+    {
+      label: 'Lunch',
+      mealType: 'lunch',
+      percentage: percentage.lunch,
+      caloriesConsumed: calsAndItemsEatenByMeal.value.lunch.calories,
+      image: '/src/assets/images/meals/lunch.png',
+      countedItems: calsAndItemsEatenByMeal.value.lunch.itemsCount
+    },
+    {
+      label: 'Dinner',
+      mealType: 'dinner',
+      percentage: percentage.dinner,
+      caloriesConsumed: calsAndItemsEatenByMeal.value.dinner.calories,
+      image: '/src/assets/images/meals/dinner.png',
+      countedItems: calsAndItemsEatenByMeal.value.dinner.itemsCount
+    },
+    {
+      label: 'Snacks',
+      mealType: 'snacks',
+      percentage: percentage.snacks,
+      caloriesConsumed: calsAndItemsEatenByMeal.value.snacks.calories,
+      image: '/src/assets/images/meals/snack.png',
+      countedItems: calsAndItemsEatenByMeal.value.snacks.itemsCount
+    }
+  ]
+})
+
+onBeforeMount(() => {
+  if (date.value) {
+    getUserDashboard(date.value)
+  }
+})
 </script>
 
 <style lang="scss" scoped>
