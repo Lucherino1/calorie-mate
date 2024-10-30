8bb6f4c102d545a01be6b15f399e7f043f197b74
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-30 02:35:06 +0200

    CM-7: added enums for products and recipes

---
+++ src/types/products-and-recipes.enums.ts
@@ -0,0 +1,24 @@
+export enum ERecipeType {
+  sweet = 'sweet',
+  soup = 'soup',
+  salad = 'salad',
+  mainCourse = 'mainCourse',
+  appetizer = 'appetizer',
+  dessert = 'dessert',
+  beverage = 'beverage',
+  snack = 'snack',
+  sideDish = 'sideDish',
+}
+
+export enum EProductType {
+  vegetable = 'vegetable',
+  fruit = 'fruit',
+  meat = 'meat',
+  dairy = 'dairy',
+  grain = 'grain',
+  seafood = 'seafood',
+  nut = 'nut',
+  spice = 'spice',
+  beverage = 'beverage',
+  condiment = 'condiment',
+}
