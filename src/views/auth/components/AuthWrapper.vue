e15ea2bf68b662162ce71dce3193575082a70e13
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-02 16:20:18 +0200

    CM-9: fixed AuthWrapper issues

--- src/views/auth/components/AuthWrapper.vue
+++ src/views/auth/components/AuthWrapper.vue
@@ -3,17 +3,15 @@
     <div class="flex">
       <div class="flex-1 flex flex-col justify-center items-center">
         <div class="flex flex-col justify-between py-10 h-full w-full max-w-[410px] px-2">
-          <span>
-            <button
-              class="flex items-center gap-1 text-gray-light hover:text-gray-dark transition-colors duration-300"
-              @click="moveToAboutUs"
-            >
-              <div class="w-6 h-6 flex justify-center items-center">
-                <IconArrowLeft />
-              </div>
-              Back
-            </button>
-          </span>
+          <router-link
+            :to="{ name: routeNames.aboutUs }"
+            class="flex items-center gap-1 text-gray-light hover:text-gray-dark transition-colors duration-300"
+          >
+            <div class="w-6 h-6 flex justify-center items-center">
+              <IconArrowLeft />
+            </div>
+            Back
+          </router-link>
           <div>
             <h2 class="text-4xl leading-[56px] font-bold text-primary-dark">{{ titleText }}</h2>
             <slot />
@@ -46,14 +44,20 @@
           <div class="flex justify-center items-center text-center mb-[100px]">
             <h1 class="text-5xl text-white"><b>CALORIE</b> MATE</h1>
           </div>
-          <button
-            class="text-white text-lg leading-[18px] px-10 lg:px-[110px]
-             py-[20px] border-2 border-gray-light rounded-[28px] sm:px-1 md:px-5
-            hover:bg-white hover:text-primary transition-all duration-300"
-            @click="moveToAboutUs"
+          <router-link
+            :to="{ name: routeNames.aboutUs }"
+            class="inline-block"
           >
-            <p class="text-nowrap leading-10"><b>Click here</b><br>to learn more about us</p>
-          </button>
+            <button
+              class="text-white text-lg leading-[18px] px-10 lg:px-[110px]
+             py-[20px] border-2 border-gray-light rounded-[28px] sm:px-1 md:px-5
+             hover:bg-white hover:text-primary transition-all duration-300"
+            >
+              <p class="text-nowrap leading-10">
+                <b>Click here</b><br>to learn more about us
+              </p>
+            </button>
+          </router-link>
         </div>
       </div>
     </div>
@@ -61,9 +65,7 @@
 </template>
 
 <script lang="ts" setup>
-import { router } from '@/router'
 import { routeNames } from '@/router/route-names'
-import IconArrowLeft from '~icons/icon/arrow-left'
 
 defineProps<{
   titleText: string
@@ -71,8 +73,4 @@ defineProps<{
   toggleAuthPageBtnText: string
   toggleAuthPage: () => void
 }>()
-
-const moveToAboutUs = () => {
-  router.push({ name: routeNames.aboutUs })
-}
 </script>
