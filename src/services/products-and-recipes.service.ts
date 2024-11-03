16ae334dc7c52b2bf9d28b2798d92924be04c09c
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-03 19:02:01 +0200

    CM-9: added methods to get user's products and recipes

--- src/services/products-and-recipes.service.ts
+++ src/services/products-and-recipes.service.ts
@@ -14,6 +14,22 @@ class ProductsAndRecipesService {
 
     return data
   }
+
+  getUserProduct = async () => {
+    const { data, error } = await useSupabase.from('user-products').select('*')
+
+    if (error) throw new Error(error.message)
+
+    return data
+  }
+
+  getUserRecipes = async () => {
+    const { data, error } = await useSupabase.from('user-recipes').select('*')
+
+    if (error) throw new Error(error.message)
+
+    return data
+  }
 }
 
 export const productsAndRecipesService = new ProductsAndRecipesService()
