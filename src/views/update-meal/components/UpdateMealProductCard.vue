64514273e1bd6fe31386e20cf34b8310bead35c8
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-03 19:28:45 +0200

    CM-9: created MealProductCard component

---
+++ src/views/update-meal/components/UpdateMealProductCard.vue
@@ -0,0 +1,91 @@
+<template>
+  <el-card class="px-2">
+    <div class="flex items-center justify-between min-h-[80px]">
+      <div class="flex min-w-[320px] items-center justify-between">
+        <div class="flex justify-center items-center">
+          <div>
+            <p class="text-wrap truncate font-bold text-base text-primary-dark">{{ product.name }}</p>
+            <span class="text-sm text-gray-light">- {{ product.type }}</span>
+          </div>
+
+          <span v-if="product.isVegan" class="flex items-center fill-success w-[20px] h-[20px] ml-1">
+            <IconVegan class="w-full h-full" />
+          </span>
+        </div>
+        <el-input-number
+          v-model="grams"
+          :min="1"
+          :size="$elComponentSize.default"
+          @input="handleInputChange"
+        >
+          <template #suffix>g</template>
+        </el-input-number>
+      </div>
+
+      <ul class="flex flex-wrap text-center gap-6 text-gray-dark text-base">
+        <li class="nutrition-list__item">
+          <p class="truncate">Carbs:</p>
+          <span class="truncate"><b>{{ calculatedNutrition.carbs }}</b> g</span>
+        </li>
+
+        <li class="nutrition-list__item">
+          <p class="truncate">Proteins:</p>
+          <span class="truncate"><b>{{ calculatedNutrition.proteins }}</b> g</span>
+        </li>
+
+        <li class="nutrition-list__item">
+          <p class="truncate">Fats:</p>
+          <span class="truncate"><b>{{ calculatedNutrition.fats }}</b> g</span>
+        </li>
+
+        <li class="nutrition-list__item">
+          <p class="truncate">Calories:</p>
+          <span class="truncate"><b>{{ calculatedNutrition.calories }}</b> kcal</span>
+        </li>
+      </ul>
+
+      <div class="my-0 flex justify-end w-[180px]">
+        <el-button
+          :type="$elComponentType.danger"
+          @click="removeProduct"
+        >
+          Remove
+        </el-button>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script lang="ts" setup>
+import IconVegan from '~icons/icon/vegan'
+import debounce from 'lodash/debounce'
+
+const props = defineProps<{
+  product: IProduct
+}>()
+
+const emit = defineEmits<{
+  (e: 'update-product', updatedProduct: IProduct)
+  (e: 'remove-product', productId: string)
+}>()
+
+const localProduct = ref({ ...props.product })
+const grams = ref(localProduct.value.grams)
+
+const calculatedNutrition = computed(() => {
+  return nutritionService.calcNutritionPerGrams(props.product.nutritionDetails, grams.value)
+})
+
+const saveChangesDebounce = debounce(() => {
+  localProduct.value.grams = grams.value
+  emit('update-product', { ...localProduct.value })
+}, 500)
+
+function handleInputChange () {
+  saveChangesDebounce()
+}
+
+function removeProduct () {
+  emit('remove-product', localProduct.value.id)
+}
+</script>
