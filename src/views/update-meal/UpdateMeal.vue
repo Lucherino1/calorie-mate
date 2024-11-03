99288b96a1253586ed44ecfacb82a73e4ef4bf92
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-03 23:52:38 +0200

    CM-9: created progress dashboard and implemented methods from update meal service

--- src/views/update-meal/UpdateMeal.vue
+++ src/views/update-meal/UpdateMeal.vue
@@ -2,9 +2,52 @@
   <div v-loading.fullscreen="pageLoading" class="app-container--dashboard h-full">
     <div class="flex h-full">
       <div class="flex-col flex gap-5 w-full">
-        <h1 class="font-bold text-primary-dark text-[34px] leading-10">
-          Update your {{ mealType }}
-        </h1>
+        <div class="flex items-center text-center justify-between">
+          <h1 class="font-bold text-primary-dark text-[34px] leading-10">
+            Update your {{ mealType }}
+          </h1>
+
+          <el-card>
+            <div class="flex text-center items-center gap-10">
+              <p class="capitalize font-bold text-primary-dark">TOTAL:</p>
+              <el-progress type="circle" :width="80" :percentage="totalCaloriesPercentage">
+                <template #default>
+                  <div class="flex items-center justify-center text-center">
+                    <span v-if="totalCaloriesPercentage > 100" class="text-base">
+                      <b>+{{ Math.abs(totalCaloriesPercentage) }}</b>%
+                      <p>Over</p>
+                    </span>
+                    <span v-else class="text-base">
+                      <b>{{ totalCaloriesPercentage }}</b>%
+                    </span>
+                  </div>
+                </template>
+              </el-progress>
+
+              <ul class="flex flex-wrap text-center gap-6 text-gray-dark text-base">
+                <li class="nutrition-list__item">
+                  <p class="truncate">Carbs:</p>
+                  <span class="truncate"><b>{{ totalNutrients.carbs }}</b> g</span>
+                </li>
+
+                <li class="nutrition-list__item">
+                  <p class="truncate">Proteins:</p>
+                  <span class="truncate"><b>{{ totalNutrients.proteins }}</b> g</span>
+                </li>
+
+                <li class="nutrition-list__item">
+                  <p class="truncate">Fats:</p>
+                  <span class="truncate"><b>{{ totalNutrients.fats }}</b> g</span>
+                </li>
+
+                <li class="nutrition-list__item">
+                  <p class="truncate">Calories:</p>
+                  <span class="truncate"><b>{{ totalNutrients.calories }}</b> kcal</span>
+                </li>
+              </ul>
+            </div>
+          </el-card>
+        </div>
 
         <div class="mx-0">
           <el-tabs v-model="activeTab" stretch class="min-h-[500px] flex">
@@ -45,11 +88,23 @@ const pageLoading = ref(false)
 const activeTab = ref('products')
 
 const userMeals = ref<IMeals>(null)
+
 const allProducts = ref<IProduct[]>([])
 const productsInMeal = ref<IProduct[]>([])
 
 const allRecipes = ref<IRecipe[]>([])
 const recipesInMeal = ref<IRecipe[]>([])
+
+const totalNutrients = computed(() => {
+  return updateMealService.calculateTotalNutrients(productsInMeal.value, recipesInMeal.value)
+})
+
+const totalCaloriesPercentage = computed(() => {
+  if (!authStore.user || !authStore.user.targetNutritionDetailsByMeal[props.mealType]) return 0
+
+  const targetCalories = authStore.user.targetNutritionDetailsByMeal[props.mealType].calories
+  return updateMealService.calculateCaloriesPercentage(targetCalories, totalNutrients.value.calories)
+})
 
 const getUserMeals = async () => {
   try {
@@ -106,6 +161,7 @@ const getAllRecipes = async () => {
 watch(activeTab, (newTab) => {
   if (newTab === 'recipes' && !allRecipes.value.length) {
     getAllRecipes()
+    console.log(userMeals.value)
   }
 })
 
