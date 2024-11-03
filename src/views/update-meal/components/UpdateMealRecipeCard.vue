e6711cd0b9a07c591bf4e8790e413becffbd98d9
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-03 19:27:29 +0200

    CM-9: created UpdateRecipeCard

---
+++ src/views/update-meal/components/UpdateMealRecipeCard.vue
@@ -0,0 +1,144 @@
+<template>
+  <el-card class="p-5 rounded-3x">
+    <div class="flex gap-10 min-h-[200px]">
+      <div class="w-[200px] h-[200px] bg-gray-200 rounded-3xl overflow-hidden">
+        <SkeletonImage :img-src="recipe.image">
+          <template #placeholder>
+            <IconErrorRecipe class="fill-gray-dark" />
+          </template>
+        </SkeletonImage>
+      </div>
+
+      <div class="flex flex-col justify-between flex-1">
+        <div class="flex justify-between items-start">
+          <div>
+            <div class="flex items-center">
+              <p class="text-2xl font-bold text-primary-dark">
+                {{ recipe.name }}
+              </p>
+              <span v-if="recipe.isVegan" class="flex items-center fill-success w-[20px] h-[20px] ml-2">
+                <IconVegan class="w-full h-full" />
+              </span>
+            </div>
+            <p class="text-gray-500 mt-1">{{ recipe.description }}</p>
+          </div>
+
+          <div class="flex items-center gap-2">
+            <span class="text-lg text-gray-500">Portions:</span>
+            <el-input-number
+              v-model="portions"
+              :min="1"
+              :size="$elComponentSize.large"
+              @input="handleInputChange"
+            />
+          </div>
+        </div>
+
+        <ul class="flex lg:gap-10 xl:gap-14 mt-5">
+          <li class="text-center">
+            <p class="text-gray-light">
+              Carbs:<br><b class="text-gray-dark text-xl">{{ calcedNutrition.carbs }}</b> g
+            </p>
+          </li>
+
+          <li class="text-center">
+            <p class="text-gray-light">
+              Proteins:<br><b class="text-gray-dark text-xl">{{ calcedNutrition.proteins }}</b> g
+            </p>
+          </li>
+
+          <li class="text-center">
+            <p class="text-gray-light">
+              Fats:<br><b class="text-gray-dark text-xl">{{ calcedNutrition.fats }}</b> g
+            </p>
+          </li>
+
+          <li class="text-center">
+            <p class="text-gray-light">
+              Calories:<br><b class="text-gray-dark text-xl">{{ calcedNutrition.calories }}</b> g
+            </p>
+          </li>
+
+          <li class="text-center">
+            <p class="text-gray-light">
+              Portion weight:<br><b class="text-gray-dark text-xl">{{ recipe.portionWeight }}</b> g
+            </p>
+          </li>
+        </ul>
+
+        <div class="flex justify-between mt-3">
+          <el-button ref="buttonRef" v-click-outside="onClickOutside">
+            Show ingredients
+          </el-button>
+
+          <el-popover
+            ref="popoverRef"
+            :virtual-ref="buttonRef"
+            trigger="click"
+            title="Ingredients:"
+            virtual-triggering
+          >
+            <div
+              v-for="ingredient in recipe.ingredients"
+              :key="ingredient.id"
+              class="flex flex-col gap-1"
+            >
+              <p>- {{ ingredient.name }}</p>
+            </div>
+          </el-popover>
+
+          <div class="flex justify-end w-[180px]">
+            <el-button
+              :type="$elComponentType.danger"
+              @click="removeRecipe"
+            >
+              Remove
+            </el-button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script lang="ts" setup>
+import debounce from 'lodash/debounce'
+import { ClickOutside as vClickOutside } from 'element-plus'
+import IconErrorRecipe from '~icons/icon/error-recipe'
+
+const props = defineProps<{
+  recipe: IRecipe
+}>()
+
+const emit = defineEmits<{
+  (e: 'update-recipe', updatedRecipe: IRecipe)
+  (e: 'remove-recipe', recipeId: string)
+}>()
+
+const originalRecipe = ref({ ...props.recipe })
+const localRecipe = ref({ ...props.recipe })
+const portions = ref(localRecipe.value.portions)
+
+const calcedNutrition = computed(() => {
+  return nutritionService.calcIngredientsNutrition(originalRecipe.value.ingredients, portions.value)
+})
+
+const saveChangesDebounce = debounce(() => {
+  localRecipe.value.portions = portions.value
+  emit('update-recipe', { ...localRecipe.value })
+}, 500)
+
+function handleInputChange () {
+  saveChangesDebounce()
+}
+
+function removeRecipe () {
+  emit('remove-recipe', localRecipe.value.id)
+}
+
+const buttonRef = ref()
+const popoverRef = ref()
+const onClickOutside = () => {
+  unref(popoverRef).popperRef?.delayHide?.()
+}
+</script>
