65c51a8ce84aaee1b32ad35b94a79aeaaeb4f8c9
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-03 19:28:02 +0200

    CM-9: created tabs components

---
+++ src/views/update-meal/components/tabs/UpdateMealProductsTab.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="max-h-[600px] rounded-lg">
+    <div class="mt-5">
+      <el-select
+        v-model="searchQuery"
+        filterable
+        :filter-method="filterProducts"
+        :size="$elComponentSize.large"
+        placeholder="Enter a product name, e.g. 'bread', 'avocado', 'yogurt'"
+        clearable
+      >
+        <el-option
+          v-for="product in filteredProducts"
+          :key="product.id"
+          :label="product.name"
+          :value="product.name"
+          @click="addProductToMeal(product)"
+        >
+          <div class="flex justify-between w-full">
+            <span class="font-semibold">{{ product.name }}</span>
+            <p><b>{{ product.nutritionDetails.calories }}</b> kcal</p>
+          </div>
+        </el-option>
+        <template #empty>No products match</template>
+      </el-select>
+    </div>
+
+    <div class="mt-8 flex flex-col gap-5">
+      <UpdateMealProductCard
+        v-for="product in productsInMeal"
+        :key="product.id"
+        :product="product"
+        @update-product="handleProductUpdate"
+        @remove-product="handleProductRemove"
+      />
+    </div>
+
+    <div v-if="productsInMeal.length > productsPerPage" class="mt-5 w-full flex items-center justify-center">
+      <el-button
+        class="min-w-[600px]"
+        type="primary"
+        :size="$elComponentSize.large"
+        @click="loadMoreProducts"
+      >
+        See more
+      </el-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { showNotification } from '@/helpers'
+
+const props = defineProps<{
+  mealType: TMealType
+  allProducts: IProduct[]
+}>()
+
+const productsInMeal = defineModel<IProduct[]>('productsInMeal')
+
+const dashboardStore = useDashboardStore()
+
+const authStore = useAuthStore()
+
+const productsPerPage = ref(10)
+
+function loadMoreProducts () {
+  productsPerPage.value += 10
+}
+
+const searchQuery = ref('')
+const filteredProducts = ref<IProduct[]>([])
+
+function filterProducts (searchQuery: string) {
+  if (!searchQuery) {
+    filteredProducts.value = props.allProducts
+  } else {
+    filteredProducts.value = props.allProducts.filter(recipe =>
+      recipe.name.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  }
+}
+
+async function addProductToMeal (product: IProduct) {
+  const isProductAlreadyInMeal = productsInMeal.value.some(prod => prod.id === product.id)
+
+  if (isProductAlreadyInMeal) {
+    showNotification('You may just change your grams', 'This product is already added to your meal.', 'warning')
+  } else {
+    const defaultGrams = 100
+    const newProduct: IProduct = { ...product, grams: defaultGrams }
+
+    productsInMeal.value.unshift(newProduct)
+    try {
+      await updateMealService.updateMeal(authStore.user.id, dashboardStore.date, props.mealType, { ...newProduct }, 'products')
+    } catch (error) {
+      showNotification()
+    }
+  }
+}
+
+async function handleProductUpdate (updatedProduct: IProduct) {
+  const index = productsInMeal.value.findIndex(product => product.id === updatedProduct.id)
+  if (index !== -1) productsInMeal.value[index] = updatedProduct
+
+  try {
+    await updateMealService.updateMeal(authStore.user.id, dashboardStore.date, props.mealType, updatedProduct, 'products')
+  } catch (error) {
+    showNotification()
+  }
+}
+
+async function handleProductRemove (productId: string) {
+  const index = productsInMeal.value.findIndex(product => product.id === productId)
+  if (index !== -1) {
+    productsInMeal.value.splice(index, 1)
+
+    try {
+      await updateMealService.removeProduct(
+        authStore.user.id,
+        dashboardStore.date,
+        props.mealType,
+        productId,
+        'products'
+      )
+    } catch (error) {
+      showNotification()
+    }
+  }
+}
+</script>
---
+++ src/views/update-meal/components/tabs/UpdateMealRecipesTab.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="max-h-[600px] rounded-lg">
+    <div class="mt-5">
+      <el-select
+        v-model="searchQuery"
+        filterable
+        :filter-method="filterRecipes"
+        :size="$elComponentSize.large"
+        placeholder="Enter a recipe name, e.g. 'chicken salad', 'strawberry cheesecake'"
+        clearable
+      >
+        <el-option
+          v-for="recipe in filteredRecipes"
+          :key="recipe.id"
+          :label="recipe.name"
+          :value="recipe.name"
+          @click="addRecipeToMeal(recipe)"
+        >
+          <div class="flex justify-between w-full">
+            <p class="font-semibold">{{ recipe.name }}</p>
+            <p>portion weight: <b>{{ recipe.portionWeight }}</b> g</p>
+          </div>
+        </el-option>
+        <template #empty>No recipes match</template>
+      </el-select>
+    </div>
+
+    <div class="mt-8 flex flex-col gap-5">
+      <UpdateMealRecipeCard
+        v-for="recipe in recipesInMeal"
+        :key="recipe.id"
+        :recipe="recipe"
+        @update-recipe="handleRecipeUpdate"
+        @remove-recipe="handleRecipeRemove"
+      />
+    </div>
+
+    <div v-if="recipesInMeal.length > recipesPerPage" class="mt-5">
+      <el-button
+        type="primary"
+        @click="loadMoreRecipes"
+      >
+        See more
+      </el-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { showNotification } from '@/helpers'
+
+const props = defineProps<{
+  mealType: TMealType
+  allRecipes: IRecipe[]
+}>()
+
+const recipesInMeal = defineModel<IRecipe[]>('recipesInMeal')
+
+const dashboardStore = useDashboardStore()
+
+const authStore = useAuthStore()
+
+const recipesPerPage = ref(5)
+
+function loadMoreRecipes () {
+  recipesPerPage.value += 5
+}
+
+const searchQuery = ref('')
+const filteredRecipes = ref<IRecipe[]>([])
+
+function filterRecipes (searchQuery: string) {
+  if (!searchQuery) {
+    filteredRecipes.value = props.allRecipes
+  } else {
+    filteredRecipes.value = props.allRecipes.filter(recipe =>
+      recipe.name.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  }
+}
+
+async function addRecipeToMeal (recipe: IRecipe) {
+  const isRecipeAlreadyInMeal = recipesInMeal.value.some(recip => recip.id === recipe.id)
+
+  if (isRecipeAlreadyInMeal) {
+    showNotification('You may just change your portion count', 'This recipe is already added to your meal.', 'warning')
+  } else {
+    const defaultPortion = 1
+    const newRecipe: IRecipe = { ...recipe, portions: defaultPortion }
+
+    recipesInMeal.value.unshift(newRecipe)
+    try {
+      await updateMealService.updateMeal(authStore.user.id, dashboardStore.date, props.mealType, newRecipe, 'recipes')
+    } catch (error) {
+      showNotification()
+    }
+  }
+}
+
+async function handleRecipeUpdate (updatedRecipe: IRecipe) {
+  const index = recipesInMeal.value.findIndex(recipe => recipe.id === updatedRecipe.id)
+  if (index !== -1) recipesInMeal.value[index] = updatedRecipe
+
+  try {
+    await updateMealService.updateMeal(authStore.user.id, dashboardStore.date, props.mealType, updatedRecipe, 'recipes')
+  } catch (error) {
+    showNotification()
+  }
+}
+
+async function handleRecipeRemove (recipeId: string) {
+  const index = recipesInMeal.value.findIndex(recipe => recipe.id === recipeId)
+  if (index !== -1) {
+    recipesInMeal.value.splice(index, 1)
+
+    try {
+      await updateMealService.removeProduct(
+        authStore.user.id,
+        dashboardStore.date,
+        props.mealType,
+        recipeId,
+        'recipes'
+      )
+    } catch (error) {
+      showNotification()
+    }
+  }
+}
+</script>
