ecd62a60e00210b7eca86877824c025412ff8023
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-04 19:43:11 +0200

    CM-9: added paginated getting products and recipes

--- src/services/products-and-recipes.service.ts
+++ src/services/products-and-recipes.service.ts
@@ -30,6 +30,28 @@ class ProductsAndRecipesService {
 
     return data
   }
+
+  getPaginatedProducts = async (limit: number, offset: number) => {
+    const { data, count, error } = await useSupabase
+      .from('products')
+      .select('*', { count: 'exact' })
+      .range(offset, offset + limit - 1)
+
+    if (error) throw new Error(error.message)
+
+    return { data, count }
+  }
+
+  getPaginatedRecipes = async (limit: number, offset: number) => {
+    const { data, count, error } = await useSupabase
+      .from('recipes')
+      .select('*', { count: 'exact' })
+      .range(offset, offset + limit - 1)
+
+    if (error) throw new Error(error.message)
+
+    return { data, count }
+  }
 }
 
 export const productsAndRecipesService = new ProductsAndRecipesService()
