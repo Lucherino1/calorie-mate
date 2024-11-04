c6495d61f244ff14f4d660d48478d36de80aff86
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-04 16:01:10 +0200

    CM-9: fixed dashboard loading

--- src/store/modules/dashboard.store.ts
+++ src/store/modules/dashboard.store.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useDashboardStore = defineStore('dashboardStore', () => {
   const route = useRoute()
-  const date = ref(route.query.date.toString() || new Date().toISOString().split('T')[0])
+  const date = ref((route.query.date || new Date().toISOString().split('T')[0]).toString())
 
   return {
     date
