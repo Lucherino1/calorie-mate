8c7c67c660bf4a59621813cf0e830a2efbb576eb
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-04 17:17:33 +0200

    CM-9: used new ProgressCalories component, removed empty states

--- src/views/update-meal/UpdateMeal.vue
+++ src/views/update-meal/UpdateMeal.vue
@@ -4,7 +4,7 @@
       <div class="flex-col flex gap-5 w-full">
         <div class="flex items-center text-center justify-between">
           <div class="flex flex-col gap-5">
-            <BackButton :date="date" :button-text="'Back to dashboard'" :route-name="$routeNames.dashboard" />
+            <BackButton :button-text="'Back to dashboard'" :route-name="$routeNames.dashboard" />
             <h1 class="font-bold text-primary-dark text-[34px] leading-10">
               Update your {{ mealType }}
             </h1>
@@ -13,7 +13,11 @@
           <el-card class="px-2">
             <div class="flex text-center items-center gap-5 ">
               <p class="capitalize font-bold text-primary-dark">TOTAL:</p>
-              <el-progress type="circle" :width="80" :percentage="totalCaloriesPercentage">
+              <ProgressCalories
+                :type="'circle'"
+                :percentage="totalCaloriesPercentage || 0"
+                :progress-width="80"
+              >
                 <template #default>
                   <div class="flex items-center justify-center text-center">
                     <span class="text-sm text-primary-dark">
@@ -22,34 +26,31 @@
                     </span>
                   </div>
                 </template>
-              </el-progress>
+              </ProgressCalories>
 
               <ul class="flex flex-wrap text-center gap-8 text-gray-dark text-base">
                 <li class="nutrition-list__item">
                   <p class="truncate">Carbs:</p>
                   <span class="truncate">
                     <b>{{ totalNutrients.carbs }}/
-                      <span class="text-sm">
-                        {{ authStore.user.targetNutritionDetailsByMeal[props.mealType].carbs }}
-                      </span></b> g
+                      {{ authStore.user.targetNutritionDetailsByMeal[props.mealType].carbs }}
+                    </b> g
                   </span>
                 </li>
 
                 <li class="nutrition-list__item">
                   <p class="truncate">Proteins:</p>
                   <span class="truncate"><b>{{ totalNutrients.proteins }}/
-                    <span class="text-sm">
-                      {{ authStore.user.targetNutritionDetailsByMeal[props.mealType].proteins }}
-                    </span></b> g
+                    {{ authStore.user.targetNutritionDetailsByMeal[props.mealType].proteins }}
+                  </b> g
                   </span>
                 </li>
 
                 <li class="nutrition-list__item">
                   <p class="truncate">Fats:</p>
                   <span class="truncate"><b>{{ totalNutrients.fats }}/
-                    <span class="text-sm">
-                      {{ authStore.user.targetNutritionDetailsByMeal[props.mealType].fats }}
-                    </span></b> g
+                    {{ authStore.user.targetNutritionDetailsByMeal[props.mealType].fats }}
+                  </b> g
                   </span>
                 </li>
               </ul>
@@ -59,34 +60,21 @@
 
         <div class="mx-0">
           <el-tabs v-model="activeTab" stretch class="min-h-[500px] flex">
-            <el-tab-pane label="Products" name="products" class="px-10 pb-10 overflow-y-scroll">
+            <el-tab-pane label="Products" name="products" class=" pb-10 overflow-y-scroll">
               <UpdateMealProductsTab
                 v-model:products-in-meal="productsInMeal"
                 :meal-type="mealType"
                 :all-products="allProducts"
                 :user-meals="userMeals"
               />
-
-              <el-empty
-                v-if="!productsInMeal.length"
-                class="text-2xl text-gray-light font-bold mt-5"
-                :image-size="200"
-                description="No products added at the moment."
-              />
             </el-tab-pane>
 
-            <el-tab-pane class="px-10 pb-10 overflow-y-scroll" label="Recipes" name="recipes">
+            <el-tab-pane class="pb-10 overflow-y-scroll" label="Recipes" name="recipes">
               <UpdateMealRecipesTab
                 v-model:recipes-in-meal="recipesInMeal"
                 :meal-type="mealType"
                 :all-recipes="allRecipes"
                 :user-meals="userMeals"
-              />
-              <el-empty
-                v-if="!recipesInMeal.length"
-                class="text-2xl text-gray-light font-bold mt-5"
-                description="No recipes added at the moment."
-                :image-size="200"
               />
             </el-tab-pane>
           </el-tabs>
@@ -103,19 +91,12 @@ const props = defineProps<{
   mealType?: TMealType
 }>()
 
-const route = useRoute()
-
 const dashboardStore = useDashboardStore()
 
 const authStore = useAuthStore()
 
 const pageLoading = ref(false)
 const activeTab = ref('products')
-
-const date = computed(() => {
-  if (dashboardStore.date) return dashboardStore.date
-  return route.query.selectedDate.toString()
-})
 
 const userMeals = ref<IMeals>(null)
 
@@ -131,17 +112,15 @@ const totalNutrients = computed(() => {
 
 const totalCaloriesPercentage = computed(() => {
   const targetNutrition = authStore.user.targetNutritionDetailsByMeal[props.mealType]
-
-  if (!authStore.user || !targetNutrition) return 0
-
   const targetCalories = targetNutrition.calories
+
   const percentage = updateMealService.calculateCaloriesPercentage(targetCalories, totalNutrients.value.calories)
   return Math.min(percentage, 100)
 })
 
 const getUserMeals = async () => {
   try {
-    const userMeal = await updateMealService.getUserMeals(authStore.user.id, date.value, props.mealType)
+    const userMeal = await updateMealService.getUserMeals(authStore.user.id, dashboardStore.date, props.mealType)
 
     userMeals.value = userMeal
     productsInMeal.value = userMeal.products.map(userProduct => ({ ...userProduct })).reverse()
@@ -205,6 +184,6 @@ onBeforeMount(() => {
 
 <style lang="scss" scoped>
 .nutrition-list__item {
-  @apply max-w-[80px] flex flex-col gap-2 text-primary-dark;
+  @apply max-w-[80px] flex gap-2 flex-col text-sm text-primary-dark;
 }
 </style>
