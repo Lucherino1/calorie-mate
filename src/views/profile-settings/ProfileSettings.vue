ce4cab1e0a2462fe138b57b6a65299f8957c698c
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-14 16:02:00 +0200

    CM-10: WIP

--- src/views/profile-settings/ProfileSettings.vue
+++ src/views/profile-settings/ProfileSettings.vue
@@ -1,28 +1,27 @@
 <template>
   <div class="app-container--compact flex flex-col h-full">
     <h1 class="w-full page-header">Profile settings</h1>
-    <div class="flex w-full justify-between overflow-hidden">
-      <div class="mx-0 flex-1">
-        <el-tabs v-model="activeTab" stretch class="flex w-full h-full">
-          <el-tab-pane lazy label="Body Details" name="bodyDetails" class="h-full flex justify-center items-center">
-            <el-card class="mt-2 h-full px-10 min-w-[600px]">
-              <UserFormBodyDetails
-                class="overflow-y-auto overflow-x-hidden"
-                :body-form-data="formData"
-                submit-button-text="Sibmit"
-              />
-            </el-card>
-          </el-tab-pane>
 
-          <el-tab-pane lazy class="h-full" label="Recipes" name="recipes">
-            <el-card class="max-w-[600px] mt-2">
-              <UserFormProfile
-                :profile-form-data="profileFormData"
-              />
-            </el-card>
-          </el-tab-pane>
-        </el-tabs>
-      </div>
+    <div class="mx-0 flex-1 flex w-full justify-between overflow-hidden">
+      <el-tabs v-model="activeTab" stretch class="flex w-full h-full">
+        <el-tab-pane lazy label="Body Details" name="bodyDetails" class="h-full py-5 flex justify-center items-center">
+          <el-card class="h-full px-10 max-w-[600px] card--no-shadow w-full">
+            <UserFormBodyDetails
+              class="overflow-y-auto w-full"
+              :body-form-data="formData"
+              submit-button-text="Save"
+            />
+          </el-card>
+        </el-tab-pane>
+
+        <el-tab-pane lazy class="h-full" label="Recipes" name="recipes">
+          <el-card class="max-w-[600px] mt-2">
+            <UserFormProfile
+              :profile-form-data="profileFormData"
+            />
+          </el-card>
+        </el-tab-pane>
+      </el-tabs>
     </div>
   </div>
 </template>
