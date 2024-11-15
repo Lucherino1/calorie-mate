eb8e044cc607be5faf159cc508ccbf3e3efe75a5
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-15 18:46:06 +0200

    Revert "CM-10: added submitBtnDisabled prop and changed styles of form to reuse the component in Profile settings"
    This reverts commit 1e446773dc61c7f1e1f503c61cb433bf56fc3475.

--- src/components/profile-details-forms/UserFormProfile.vue
+++ src/components/profile-details-forms/UserFormProfile.vue
@@ -3,53 +3,48 @@
     ref="formRef"
     :rules="formRules"
     :model="localProfileFormModel"
-    class="mt-[20px] flex flex-col gap-2 overflow-hidden"
+    class="mt-[20px] flex flex-col gap-2"
     label-position="top"
     label-width="auto"
     require-asterisk-position="right"
     :size="$elComponentSize.large"
     @submit.prevent="submit"
   >
-    <div class="h-full overflow-y-auto">
-      <el-form-item label="First name" prop="firstName">
-        <el-input v-model="localProfileFormModel.firstName" placeholder="Anthony" type="text" />
-      </el-form-item>
+    <el-form-item label="First name" prop="firstName">
+      <el-input v-model="localProfileFormModel.firstName" placeholder="Anthony" type="text" />
+    </el-form-item>
 
-      <el-form-item label="Last name" prop="lastName">
-        <el-input v-model="localProfileFormModel.lastName" placeholder="Soprano" type="text" />
-      </el-form-item>
+    <el-form-item label="Last name" prop="lastName">
+      <el-input v-model="localProfileFormModel.lastName" placeholder="Soprano" type="text" />
+    </el-form-item>
 
-      <el-form-item label="Email" prop="email">
-        <el-input v-model="localProfileFormModel.email" placeholder="email@example.com" type="email" />
-      </el-form-item>
+    <el-form-item label="Email" prop="email">
+      <el-input v-model="localProfileFormModel.email" placeholder="email@example.com" type="email" />
+    </el-form-item>
 
-      <el-form-item label="Password" prop="password" required>
-        <el-input
-          v-model="localProfileFormModel.password"
-          placeholder="Min. 8 characters"
-          type="password"
-          show-password
-        />
-      </el-form-item>
-
-      <el-form-item
-        label="Confirm Password"
-        prop="confirmPassword"
-      >
-        <el-input
-          v-model="localProfileFormModel.confirmPassword"
-          type="password"
-          placeholder="Min. 8 characters"
-        />
-      </el-form-item>
-    </div>
-
+    <el-form-item label="Password" prop="password" required>
+      <el-input
+        v-model="localProfileFormModel.password"
+        placeholder="Min. 8 characters"
+        type="password"
+        show-password
+      />
+    </el-form-item>
+    <el-form-item
+      label="Confirm Password"
+      prop="confirmPassword"
+    >
+      <el-input
+        v-model="localProfileFormModel.confirmPassword"
+        type="password"
+        placeholder="Min. 8 characters"
+      />
+    </el-form-item>
     <el-form-item>
       <slot name="submitBtn">
         <el-button
           native-type="submit"
           class="w-full"
-          :disabled="isSubmitButtonDisabled"
           :type="$elComponentType.primary"
           :size="$elComponentSize.large"
         >
@@ -61,12 +56,9 @@
 </template>
 
 <script lang="ts" setup>
-const props = withDefaults(defineProps<{
+const props = defineProps<{
   profileFormData: IProfileFields
-  isSubmitButtonDisabled?: boolean
-}>(), {
-  isSubmitButtonDisabled: false
-})
+}>()
 
 const emit = defineEmits<{
   (e: 'submit', localProfileFormModel: IProfileFields): IProfileFields
