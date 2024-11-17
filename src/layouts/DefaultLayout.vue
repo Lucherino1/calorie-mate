f47f7f182f79b3c21d138af0f940187e73e696aa
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-11-17 14:09:37 +0200

    (fix): fixed loading

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-loading.fullscreen="loading" class="w-full h-full flex bg-gray-ultra-light">
-    <Sidebar v-if="!loading" class="fixed z-50" />
-    <div v-if="!loading" class="flex-1 h-full overflow-x-hidden ml-[310px]">
+  <div v-loading="loading" class="w-full h-full flex bg-gray-ultra-light">
+    <Sidebar class="fixed z-50" />
+    <div class="flex-1 h-full overflow-x-hidden ml-[310px]">
       <slot />
     </div>
   </div>
